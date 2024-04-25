0669338f0305b61495717f71fe621054d752781c
Fabricio Belam <36649120+fbelam@users.noreply.github.com>
2024-04-24 22:21:11 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import colorsDoKarai from ".//colors-1383652.png"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={colorsDoKarai} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
